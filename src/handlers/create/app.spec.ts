0449de9c9a5a7ec8d0e898e614e6ed1b8190639c
Jesús Quirós <jquirossoto@gmail.com>
2022-02-17 17:01:23 -0600

    resets test

--- src/handlers/create/app.spec.ts
+++ src/handlers/create/app.spec.ts
@@ -1,9 +1,4 @@
-import { handler } from './app';
-import BookRepository from '../../layers/common/repositories/book.repository';
-import { Book } from '../../layers/common/utils/definitions';
 import { v4 as uuid } from 'uuid';
-
-jest.mock('../../layers/common/repositories/book.repository');
 
 describe('Create', () => {
     describe('Handler', () => {
