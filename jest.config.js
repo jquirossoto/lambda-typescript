5eb4407e5e2c99849ce020fa35073bf812f0be3d
Jesús Quirós <jquirossoto@gmail.com>
2022-02-17 12:36:50 -0600

    adds jest config

---
+++ jest.config.js
@@ -0,0 +1,36 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    verbose: false,
+    roots: ['<rootDir>/src'],
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest'
+    },
+    testMatch: ['**/?(*.)+(spec|test).+(ts|tsx|js)'],
+    collectCoverageFrom: ['src/**/*.ts*'],
+    coverageThreshold: {
+        global: {
+            branches: 0,
+            functions: 80,
+            lines: 80,
+            statements: 80
+        }
+    },
+    coverageReporters: ['json', 'lcov', 'text', 'clover'],
+    coverageDirectory: 'jest/coverage',
+    reporters: [
+        'default',
+        [
+            'jest-junit',
+            {
+                outputDirectory: 'jest/junit',
+                outputName: 'junit.xml'
+            }
+        ]
+    ],
+    moduleDirectories: ['node_modules', 'src/layers/dependencies/nodejs/node_modules'],
+    moduleNameMapper: {
+        '^/opt/(.*)$': ['<rootDir>/src/layers/common/$1']
+    }
+};
