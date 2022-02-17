954183b3ee6ff9e537a33b4662e3e92a5a8b2a71
Jesús Quirós <jquirossoto@gmail.com>
2022-02-17 12:59:35 -0600

    formats imports

--- src/handlers/update/app.ts
+++ src/handlers/update/app.ts
@@ -1,9 +1,9 @@
+import middy from '@middy/core';
 import doNotWaitForEmptyEventLoop from '@middy/do-not-wait-for-empty-event-loop';
 import httpErrorHandler from '@middy/http-error-handler';
 import httpJsonBodyParser from '@middy/http-json-body-parser';
 import httpResponseSerializer from '@middy/http-response-serializer';
 import { Callback, Context, Handler } from 'aws-lambda';
-import middy from '@middy/core';
 
 import BookRepository from '/opt/repositories/book.repository';
 import { buildSuccessReponse } from '/opt/utils/api.utils';
