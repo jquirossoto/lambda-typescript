1b3abbe2cd6736ebd4594d56415ddc01ba69abcd
Jesús Quirós <jquirossoto@gmail.com>
2022-02-19 10:09:34 -0600

    sets default value to generci in result interface

--- src/layers/common/definitions/api-gateway-proxy-result.interface.ts
+++ src/layers/common/definitions/api-gateway-proxy-result.interface.ts
@@ -2,6 +2,6 @@ import { APIGatewayProxyResult as AWSAPIGatewayProxyResult } from 'aws-lambda';
 
 import APIGatewayResponse from './api-gateway-response.interface';
 
-export default interface APIGatewayProxyResult<T> extends Omit<AWSAPIGatewayProxyResult, 'body'> {
+export default interface APIGatewayProxyResult<T = string> extends Omit<AWSAPIGatewayProxyResult, 'body'> {
     body: APIGatewayResponse<T>;
 }
